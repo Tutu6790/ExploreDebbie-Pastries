0141b3e3c642895d2efaeb932eaeb12b1996f6bd
Tutu6790 <okonudeborah28@gmail.com>
2022-07-22 21:23:09 +0100

    added more products

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 $('.carousel').carousel({
   interval: 2000
 });
+
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {
+    scrollFunction()
+};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    }
+    else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    window.scroll(0,0);
+}
